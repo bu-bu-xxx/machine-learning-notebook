b2d9cb5041caa21e1abb7c7e7ab31fadcd82f7a5
bubu_lamb <779117133@qq.com>
2024-07-18 18:12:34 +0800

    完成 linear regression.ipynb logistic regression.ipynb 开始 logistic regression.py

---
+++ code/numpy/logistic_regression.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+
+class LogisticRegression:
+    def __init__(self):
+        
+        pass
+    
+    def fit(self, X, y):
+        pass
+    
+    def predict(self, X):
+        pass
+    
+    def score(self, X, y):
+        pass
+
+
